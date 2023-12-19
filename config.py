0212d7ed5be8499c7f4a0c7ddf3b22f724efe1b2
JW <65707622+cheokjw@users.noreply.github.com>
2023-12-19 11:44:11 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cjnrny5lgrdk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cheokjiawei-employee"
+customregion = "us-east-1"
+    
